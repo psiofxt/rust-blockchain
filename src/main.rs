f41c7abce9b08849acb446089a5bea4784a972d6
Andrew <arellok@gmail.com>
2018-10-27 23:50:16 -0700

    Add skeletal GET

--- src/main.rs
+++ src/main.rs
@@ -52,13 +52,16 @@ fn handle_404() -> FutureResult<hyper::Response, hyper::Error> {
 
 =======
 
-//use hyper::{StatusCode};
-//use hyper::Method::{Get, Post};
+#[macro_use]
+extern crate serde_json;
+
+use hyper::{StatusCode};
+use hyper::Method::{Get};
 use hyper::server::{Request, Response, Service};
-//use hyper::header::{ContentLength, ContentType};
+use hyper::header::{ContentLength, ContentType};
 
 //use futures::Stream;
-use futures::future::{Future};
+use futures::future::{Future, FutureResult};
 
 mod blockchain;
 use blockchain::{Blockchain, init};
@@ -67,6 +70,33 @@ use blockchain::{Blockchain, init};
 struct Microservice;
 >>>>>>> Migrate to lower level hyper
 
+fn make_error_response(error_message: &str) -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = json!({
+        "error": error_message
+    }).to_string();
+    let response = Response::new()
+        .with_status(StatusCode::InternalServerError)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    debug!("{:?}", response);
+    futures::future::ok(response)
+}
+
+
+fn handle_404() -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = json!({
+        "error": "Route not found"
+    }).to_string();
+    let response = Response::new()
+        .with_status(StatusCode::NotFound)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    futures::future::ok(response)
+}
+
+
 impl Service for Microservice {
     type Request = Request;
     type Response = Response;
@@ -74,6 +104,9 @@ impl Service for Microservice {
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
     fn call(&self, request: Request) -> Self::Future {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Add skeletal GET
         match (request.method(), request.path()) {
             (&Get, "/") => {
                 info!("Microservice received a request: {:?}", request);
@@ -85,10 +118,13 @@ impl Service for Microservice {
                 Box::new(response)
             }
         }
+<<<<<<< HEAD
 =======
         info!("Microservice received a request: {:?}", request);
         Box::new(futures::future::ok(Response::new()))
 >>>>>>> Migrate to lower level hyper
+=======
+>>>>>>> Add skeletal GET
     }
 }
 
@@ -109,6 +145,10 @@ fn main() {
     let chain: Blockchain = init();
     info!("Blockchain: {:?}", chain);
     info!("Running microservice at {}", address);
+<<<<<<< HEAD
 >>>>>>> Migrate to lower level hyper
+=======
+    info!("~~Bockchain service successfully started~~");
+>>>>>>> Add skeletal GET
     server.run().unwrap();
 }
